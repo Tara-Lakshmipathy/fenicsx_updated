8957f81fe205cbb3a7d69ccda56b82cb79a9413a
Tarakeshwar Lakshmipathy <87063936+Tara-Lakshmipathy@users.noreply.github.com>
2025-04-30 17:16:39 +0200

    Add files via upload

---
+++ pyiron_nodes/__init__.py
@@ -0,0 +1,5 @@
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+
+from pyiron_nodes import atomistic, databases, atomistic_codes, lammps, math, plotting, continuum_fem, large_scale_atomistic
